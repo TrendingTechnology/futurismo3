5e8ae980e35db6ef595d93ebb3d16832e155f188
tsu-nera <fox10225fox@gmail.com>
2018-08-08 22:05:16 +0900

    temp

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,31 @@
+const query = `{
+  algolia: allMarkdownRemark(filter: {frontmatter: {url: {regex: "/^(?!.*(6389|6285|2935|2259|2200|6682|2538|1283|2482|2872|2623|2700|6389|2427|2091|2439|2503|6378)).*$/"}}}) {
+    edges {
+      node {
+        objectID: id
+        fields {
+          slug
+        }
+        internal {
+          content
+        }
+        frontmatter {
+          title
+          description
+          tags
+        }
+      }
+    }
+  }
+}`
+
+const queries = [
+  {
+    query,
+    transformer: ({ data }) => data.algolia.edges.map(({ node }) => node), // optional
+  },
+]
+
 module.exports = {
   siteMetadata: {
     title: `Futurismo`,
@@ -108,5 +136,17 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+
+    /* Others */
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: '1XCOHKDD5L',
+        apiKey: '431167421af36a57af839c2196b1ff78',
+        indexName: 'futurismo', // for all queries
+        queries,
+        chunkSize: 20000, // default: 1000
+      },
+    },
   ],
 }
