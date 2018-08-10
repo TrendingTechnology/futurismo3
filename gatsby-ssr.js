fb03c5ef98e709044a11541c69fd0371cc1dfaa8
Tsunemichi Harada <fox10225fox@gmail.com>
2018-08-11 00:04:58 +0900

    addthis test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,10 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react'
+
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents(
+    <script
+      type="text/javascript"
+      src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b6d7b1eced93fe6"
+    />
+  )
+}
