39207e40a809c21489b0e1bcfa1e6e3beb9fd66f
Tsunemichi Harada <fox10225fox@gmail.com>
2018-08-15 03:55:51 +0900

    no trailing slashes

--- gatsby-node.js
+++ gatsby-node.js
@@ -198,3 +198,23 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+// https://github.com/gatsbyjs/gatsby/tree/master/examples/no-trailing-slashes
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions
+
+  return new Promise((resolve, reject) => {
+    // Remove trailing slash
+    const newPage = Object.assign({}, page, {
+      path: page.path === `/` ? page.path : page.path.replace(/\/$/, ``),
+    })
+    if (newPage.path !== page.path) {
+      // Remove the old page
+      deletePage(page)
+      // Add the new page
+      createPage(newPage)
+    }
+
+    resolve()
+  })
+}
