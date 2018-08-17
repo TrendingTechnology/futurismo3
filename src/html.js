1bf772266321fbdb044b44e8cca7c53804a9ef33
Tsunemichi Harada <fox10225fox@gmail.com>
2018-08-17 18:26:04 +0900

    fix error

--- src/html.js
+++ src/html.js
@@ -8,7 +8,10 @@ export default class HTML extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1"
+          />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
