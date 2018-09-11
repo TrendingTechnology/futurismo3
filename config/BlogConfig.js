304b9907d0fa5a0ece920f96e3821e95f85a8ec6
Tsunemichi Harada <fox10225fox@gmail.com>
2018-09-12 05:17:51 +0900

    minor fix

--- config/BlogConfig.js
+++ config/BlogConfig.js
@@ -18,7 +18,7 @@ module.exports = {
   userAvatar:
     'https://res.cloudinary.com/tsu-nera/image/upload/ar_1:1,b_rgb:ffffff,bo_0px_solid_rgb:ffffff,c_fill,g_auto,r_max,w_100/v1533897547/futurismo/logo.jpg',
   userDescription:
-    'IT企業の組込みエンジニア→18年6月退職→Web未経験からフリーランスWebエンジニア目指して勉強中の31歳。Rails/React/Java/Python/C言語/Linux。今日も一日、がんばるぞい。',
+    'IT企業の組込みエンジニア→18年6月退職→Webエンジニア目指して勉強中の31歳。Rails/React/Java/Python/C言語/Linux。今日も一日、がんばるぞい。',
   userTwitterLink: 'https://twitter.com/tsu_nera',
   userGitHubLink: 'https://github.com/tsu-nera',
 }
